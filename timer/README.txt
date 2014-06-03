9d1ce88f761e44bb17e14c9b83ca7c621183037a
Hemanth Kini <hemanthkini@cmu.edu>
2014-06-02 19:52:56 -0700

    updated csv-tester to output utilization stats

--- timer/README.txt
+++ timer/README.txt
@@ -1,18 +1,14 @@
 tester.py: first version of testing code, outputting files for copying into google docs
-DONE
 
 tester2.py: revised with nicer output in terminal
-DONE
 
 script-tester2.py: version of tester2.py to support scripts via ./runbdd
-DONE
 
 csv-tester.py: rewrite for outputting comma-separated value files, for auto-import
 into google docs. will support both scripts and fork, and getopt.
 
-options to implement:
-input/output file picking DONE
-distributed, local, and cudd DONE
-timing in python vs. parsing the delta time output from runbdd DONE
-
-DONE
+some features:
+input/output file picking
+distributed, local, and cudd
+timing in python vs. parsing the delta time output from runbdd
+grabbing ite/memory utilization
--- timer/csv-tester.py
+++ timer/csv-tester.py
@@ -8,15 +8,84 @@ hostStr = ''
 numTrials = 1
 useDeltaTime = False
 verbosity = 0
+getUtilDetailsBool = False
+numDetails = 3
+details = []
+
+def initUtilizationDetails(timeFile, p):
+    global verbosity, details
+    p.stdin.write("flush")
+    p.stdin.write("\n")
+    readStr = "s"
+    while ((readStr.startswith('Unary stores')) == False and len(readStr) != 0):
+        readStr = p.stdout.readline().lstrip('cmd>')
+        if (readStr.startswith('Allocated cnt/bytes')):
+            readStr = readStr.lstrip('Allocated cnt/bytes')
+            (temp1, data, temp2) = readStr.split('/', 2)
+            (data, temp1) = data.split('.', 1)
+            details.append(int(data))
+        elif (readStr.startswith('ITEs. Total ')):
+            readStr = readStr.lstrip('ITEs. Total ')
+            print(readStr)
+            (total, local, cache, recursion) = readStr.split('.', 3)
+            total = total.lstrip(' Total ')
+            print(total)
+            details.append(int(total))
+        elif (readStr.startswith('Peak ITEc entries')):
+            readStr = readStr.lstrip('Peak ITEc entries')
+            (temp1, temp2, minimum, maximum, average, Sum) = readStr.split(':', 5)
+            average = average.lstrip().rstrip("Sum").rstrip()
+            details.append(float(average))
+
+
+def printUtilizationDetails(timeFile, p):
+    global verbosity, details
+    p.stdin.write("flush")
+    p.stdin.write("\n")
+    readStr = ""
+    while ((readStr.startswith('Unary stores')) == False):
+        readStr = p.stdout.readline().lstrip('cmd>')
+        if (readStr.startswith('Allocated cnt/bytes')):
+            readStr = readStr.lstrip('Allocated cnt/bytes')
+            (temp1, data, temp2) = readStr.split('/', 2)
+            (data, temp1) = data.split('.', 1)
+            timeFile.write(str(int(data) - int(details.pop(0))))
+            timeFile.write(',')
+        elif (readStr.startswith('ITEs. Total ')):
+            readStr = readStr.lstrip('ITEs.')
+            print(readStr)
+            (total, local, cache, recursion) = readStr.split('.', 3)
+            total = total.lstrip(' Total ')
+            print(total)
+            timeFile.write(str(int(total) - int(details.pop(0))))
+            timeFile.write(',')
+        elif (readStr.startswith('Peak ITEc entries')):
+            readStr = readStr.lstrip('Peak ITEc entries')
+            (temp1, temp2, minimum, maximum, average, Sum) = readStr.split(':', 5)
+            average = average.lstrip().rstrip("Sum").rstrip()
+            timeFile.write(str(float(average) - float(details.pop(0))))
+            timeFile.write(',')
+
+
+
+def utilizationDetailsHeader(timeFile):
+    timeFile.write("Allocated bytes from previous operation,")
+    timeFile.write("Total number of ITEs,")
+    timeFile.write("Avg Peak ITEc entries,")
 
 
 def runTests(inputFile, timeFile, p):
     global useDeltaTime, numTrials, verbosity
     for line in inputFile:
-        timeFile.write(line.rstrip() + ",")
+        timeFile.write(line.rstrip() + ",,")
         for i in range(numTrials):
             if (verbosity > 0):
                 print(line.rstrip())
+
+            # if we want to track utilization details
+            if getUtilDetailsBool:
+                initUtilizationDetails(timeFile, p)
+
             # we keep track of the number of times that
             # time is called, so that we know when to
             # stop reading STDOUT (we check for the
@@ -74,20 +143,27 @@ def runTests(inputFile, timeFile, p):
                 file.close(tempFile)
 
             if (useDeltaTime):
-                timeFile.write("," + ('%.4f' % deltaTime))
+                timeFile.write(('%.4f' % deltaTime) + ",")
             else:
-                timeFile.write("," + ('%.4f' % diffTime))
+                timeFile.write(('%.4f' % diffTime) + ",")
+
+            if getUtilDetailsBool:
+                printUtilizationDetails(timeFile, p)
+
         timeFile.write("\r\n")
 
 def runTimer(runOptions):
     global useDeltaTime, inputFileName, outputFileName
-    global portStr, hostStr, numTrials
+    global portStr, hostStr, numTrials, getUtilDetailsBool
 
     timeFile = open(outputFileName, 'w')
 
-    timeFile.write(",")
+    timeFile.write(",,")
     for i in range(numTrials):
-        timeFile.write("(seconds),Trial " + str(i + 1))
+        timeFile.write("Trial " + str(i + 1) + " (s),")
+        if (getUtilDetailsBool):
+            utilizationDetailsHeader(timeFile)
+
     timeFile.write("\r\n")
 
 
@@ -103,15 +179,21 @@ def runTimer(runOptions):
             modeStr = "Distributed Refs"
         elif (opt == "-l"):
             modeStr = "Local Refs"
-        timeFile.write(modeStr + ",")
+        timeFile.write(modeStr + ",,")
         for i in range(numTrials):
             timeFile.write(",")
+            if (getUtilDetailsBool):
+                for i in range(numDetails):
+                    timeFile.write(",")
         timeFile.write("\r\n")
 
         #write empty row
-        timeFile.write(",")
+        timeFile.write(",,")
         for i in range(numTrials):
             timeFile.write(",")
+            if (getUtilDetailsBool):
+                for i in range(numDetails):
+                    timeFile.write(",")
         timeFile.write("\r\n")
 
         # build list of arguments
@@ -130,9 +212,13 @@ def runTimer(runOptions):
         p.kill()
         file.close(inputFile)
         # create empty row
-        timeFile.write(",")
+        timeFile.write(",,")
         for i in range(numTrials):
             timeFile.write(",")
+            if (getUtilDetailsBool):
+                for i in range(numDetails):
+                    timeFile.write(",")
+
         timeFile.write("\r\n")
 
     file.close(timeFile)
@@ -154,11 +240,12 @@ def usage():
     print("\t-d               Uses the distributed package for timing. Default: enabled.")
     print("\t-l               Uses the local refs for timing. Default: disabled.")
     print("\t-v VERBOSITY     Verbose mode. Prints output. Level 1: Prints individual commands and times; Level 2: Prints everything. Default: 0.")
+    print("\t-u UTIL DETAILS  1 to list utilization details (peak bytes, peak ITEs, etc.) Default: 0")
     print("")
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "H:P:dcli:o:t:n:hv:", [])
+        opts, args = getopt.getopt(sys.argv[1:], "H:P:dcli:o:t:n:hv:u:", [])
     except getopt.GetoptError as err:
         print(err)
         usage()
@@ -166,6 +253,7 @@ def main():
 
     global useDeltaTime, inputFileName, outputFileName
     global portStr, hostStr, numTrials, verbosity
+    global getUtilDetailsBool
     runOptions = []
 
     for opt, arg in opts:
@@ -193,6 +281,9 @@ def main():
             sys.exit(0)
         elif opt == "-v":
             verbosity = int(arg)
+        elif opt == "-u":
+            if (int(arg) == 1):
+                getUtilDetailsBool = True
         else:
             print("Invalid option.")
             usage()
