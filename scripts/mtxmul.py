5cff7db763336b8d6af85cb0ec405c7695d8adce
Randal Bryant <bryant@mini.home>
2019-04-30 08:59:58 -0400

    Implemented preliminary version of matrix multiplication

--- scripts/mtxmul.py
+++ scripts/mtxmul.py
@@ -2,6 +2,8 @@
 # expressing different ways to multiply matrices with < n^3 multiplications
 
 import sys
+import getopt
+
 import circuit
 
 # Sequence of digits starting at one
@@ -23,39 +25,72 @@ def gamma(r, c, l, negate=False):
     return mmterm('gamma', r, c, l, negate)
 
 def bterm(prefix, i1, i2, j1, j2, k1, k2, l = None):
-    name = "%s-%d.%d.%d.%d.%d.%d" % (prefix, i1, i2, j1, j2, k1, k2)
+    name = "%s-%s.%s.%s.%s.%s.%s" % (prefix, i1, i2, j1, j2, k1, k2)
     if l is not None:
         name += ".l-%d" % l
     return name
 
-def brentName(i1, i2, j1, j2, k1, k2):
-    return bterm('brent', i1, i2, j1, j2, k1, k2)
+def brentName(indices):
+    t = ['*'] * 6
+    for i in range(len(indices)):
+        t[i] = str(indices[i])
+    return bterm('brent', t[0], t[1], t[2], t[3], t[4], t[5])
 
-def generateBrent(ckt, p, i1, i2, j1, j2, k1, k2):
+def generateBrent(ckt, p, indices):
+    i1, i2, j1, j2, k1, k2 = indices
     kd = i2 == j1 and i1 == k1 and j2 == k2
-    ckt.comment("Brent equation for i1 = %d, i2 = %d, j1 = %d, j2 = %d, k1 = %d, k2 = %d (k delta = %d)" % (i1, i2, j1, j2, k1, k2, 1 if kd else 0))
+    ckt.comment("Brent equation for i1 = %d, i2 = %d, j1 = %d, j2 = %d, k1 = %d, k2 = %d (kron delta = %d)" % (i1, i2, j1, j2, k1, k2, 1 if kd else 0))
     av = ckt.addVec(circuit.Vec([alpha(i1, i2, l) for l in unitRange(p)]))
     bv = ckt.addVec(circuit.Vec([beta(j1, j2, l) for l in unitRange(p)]))
     gv = ckt.addVec(circuit.Vec([beta(k1, k2, l) for l in unitRange(p)]))
     pv = ckt.addVec(circuit.Vec([bterm('bp', i1, i2, j1, j2, k1, k2, l) for l in unitRange(p)]))
     ckt.andV(pv, [av, bv, gv])
     rv = pv.concatenate(circuit.Vec([ckt.one])) if not kd else pv.dup()
-    bn = brentName(i1, i2, j1, j2, k1, k2)
+    bn = brentName([i1, i2, j1, j2, k1, k2])
     ckt.xorN(bn, rv)
     ckt.decRefs([pv])
 
-def generateAllBrents(ckt, p, n1, n2, n3):
-    for i1 in unitRange(n1):
-        for i2 in unitRange(n2):
-            for j1 in unitRange(n2):
-                for j2 in unitRange(n3):
-                    for k1 in unitRange(n1):
-                        for k2 in unitRange(n3):
-                            generateBrent(ckt, p, i1, i2, j1, j2, k1, k2)
 
-def brentReduce(ckt, n1, n2, n3, levels = [true, true, true, true, true, true]):
-    pass
+def iexpand(rlist, sofar = [[]]):
+    if len(rlist) == 0:
+        return sofar
+    n = rlist[-1]
+    nsofar = []
+    for idx in unitRange(n):
+        for l in sofar:
+            nsofar.append([idx] + l)
+    return iexpand(rlist[:-1], nsofar)
+    
 
+
+def mencode(ckt, p, n1, n2, n3):
+    ranges = [n1, n2, n2, n3, n1, n2]
+    indices = iexpand(ranges)
+    ckt.comment("Generate all Brent terms")
+    for i in indices:
+        generateBrent(ckt, p, i)
+    names = circuit.Vec([brentName(i) for i in indices])
+    ckt.comment("Find size of typical Brent term")
+    ckt.information(circuit.Vec([names[0]]))
+    ckt.comment("Find combined size of all Brent terms")
+    ckt.information(names)
+    for level in unitRange(6):
+        ckt.comment("Combining terms at level %d" % level)
+        gcount = ranges[-1]
+        ranges = ranges[:-1]
+        indices = iexpand(ranges)
+        for i in indices:
+            args = ckt.addVec(circuit.Vec([brentName(i + [x]) for x in unitRange(gcount)]))
+            bn = brentName(i)
+            ckt.andN(bn, args)
+            ckt.decRefs([args])
+        names = circuit.Vec([brentName(i) for i in indices])
+        if len(names) > 1:
+            ckt.comment("Find size of typical function at level %d" % level)
+            ckt.information(circuit.Vec([names[0]]))
+        ckt.comment("Find combined size for terms at level %d" % level)
+        ckt.information(names)
+        
 def solveMatrix(p, n1, n2, n3, f = sys.stdout, zdd = circuit.Z.none):
     ckt = circuit.Circuit(f)
     ckt.comment("Solving Brent equations to derive matrix multiplication scheme")
@@ -65,6 +100,10 @@ def solveMatrix(p, n1, n2, n3, f = sys.stdout, zdd = circuit.Z.none):
         # Declare variables for each auxilliary variable l
         ckt.comment("Variables for auxilliary variable %d" % l)
         nrow = n1
+        ncol = n3
+        gv = circuit.Vec([gamma(i/ncol+1, (i%ncol)+1, l) for i in range(nrow*ncol)])
+        ckt.declare(gv)
+        nrow = n1
         ncol = n2
         av = circuit.Vec([alpha(i/ncol+1, (i%ncol)+1, l) for i in range(nrow*ncol)])
         ckt.declare(av)
@@ -72,10 +111,8 @@ def solveMatrix(p, n1, n2, n3, f = sys.stdout, zdd = circuit.Z.none):
         ncol = n3
         bv = circuit.Vec([beta(i/ncol+1, (i%ncol)+1, l) for i in range(nrow*ncol)])
         ckt.declare(bv)
-        nrow = n1
-        ncol = n3
-        gv = circuit.Vec([beta(i/ncol+1, (i%ncol)+1, l) for i in range(nrow*ncol)])
-        ckt.declare(gv)
-        generateAllBrents(ckt, p, n1, n2, n3)
+    mencode(ckt, p, n1, n2, n3)
+    bv = circuit.Vec([brentName([])])
+    ckt.count(bv)
+    ckt.status()
 
-    
