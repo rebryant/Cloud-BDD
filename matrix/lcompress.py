728356a30157fe2248cbab092db384e0cf299435
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2019-11-14 10:25:14 -0500

    Implemented command line options

--- matrix/lcompress.py
+++ matrix/lcompress.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python
 
 # Given set of literals, compress into set with one less level
-# This should be run within the directory mm-data/compress-levels
+# for all p(p-1)/2 possible pairs of levels
 
 import sys
 import circuit
 import brent
+import getopt
+
+def usage(name):
+    print("Usage: %s [-h] [-r ROOT] [-L LFILE] [-m MODE] [-p AUX]" % name)
+    print("    -h       Print this message")
+    print("    -r ROOT  Set root name of literal file")
+    print("    -L LFILE Specify name of source literal file")
+    print("    -m MODE  Specify level merging mode")
+    print("    -p AUX   Set number of auxilliary variables in source")
+
 
 root = "smirnov"
 infile = "smirnov-family.lit"
-
-
+mode = 2
 dim = (3,3,3)
 sourceAuxCount = 23
 
 ckt = circuit.Circuit()
 
 def outName(root, fromLevel, toLevel):
-    return "%s-m%.2d+%.2d.lit" % (root, fromLevel, toLevel)
+    return "%s-m%.2d+%.2d-mode%d.lit" % (root, fromLevel, toLevel, mode)
 
-def generate(scheme, fromLevel, toLevel, root):
-    snew = scheme.mergeLevels(fromLevel, toLevel)
+def generate(scheme, fromLevel, toLevel):
+    snew = scheme.mergeLevels(fromLevel, toLevel, mode)
     fname = outName(root, fromLevel, toLevel)
     try:
         outf = open(fname, 'w')
@@ -33,7 +42,21 @@ def generate(scheme, fromLevel, toLevel, root):
     outf.close()
     print("Wrote %d literals to %s" % (len(lits), fname))
     
-def run():
+def run(name, args):
+    global root, infile, mode, sourceAuxCount
+    optlist, args = getopt.getopt(args, 'hr:L:m:p:')
+    for (opt, val) in optlist:
+        if opt == '-h':
+            usage(name)
+            return
+        elif opt == '-r':
+            root = val
+        elif opt == '-L':
+            infile = val
+        elif opt == '-m':
+            mode = int(val)
+        elif opt == '-p':
+            sourceAuxCount = int(val)
     try:
         scheme = brent.MScheme(dim, sourceAuxCount, ckt).parseLiteralsFromFile(infile)
     except Exception as ex:
@@ -41,8 +64,8 @@ def run():
         return
     for toLevel in brent.unitRange(sourceAuxCount):
         for fromLevel in range(toLevel+1, sourceAuxCount+1):
-            generate(scheme, fromLevel, toLevel, root)
+            generate(scheme, fromLevel, toLevel)
         
 if __name__ == "__main__":
-    run()
+    run(sys.argv[0], sys.argv[1:])
 
