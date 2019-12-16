3a7e0a0d829c38edbfeb5d21e32c795f1e72f551
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2019-12-16 10:01:51 -0500

    More analysis flexibility

--- matrix/mm-data/compress-literals-m2/analyze.py
+++ matrix/mm-data/compress-literals-m2/analyze.py
@@ -16,9 +16,29 @@
 
 import sys
 import re
+import getopt
+
+def usage(name):
+    print("%s: [-h] [-u] [-r a|m|c] f1.log f2.log ..." % name)
+    print(" -h      Print this message")
+    print(" -u      Uniform time format (for charting with Excel)")
+    print(" -r RPT  Specify reporting parameter; a (all combined), m (max), c (computed)")
 
 totalConjuncts = 81
+# Choices are 'combined', 'max', or 'computed'
 reportField = 'combined'
+resolution = 10.0
+floatFormat = "%.0f"
+# In uniformTime format, create uniform axis for all time points
+# Otherwise, track each time for each series
+uniformTime = False
+
+# Are we in a pass to collect all time points?
+recordTime = True
+maxTime = 0
+timePoints = []
+
+
 
 # Expressions
 initialMatcher = re.compile("Elapsed time ([0-9]+\.[0-9]+).  Initial simplification.  Total ([0-9]+) .*  Max ([0-9]+)")
@@ -28,20 +48,25 @@ timeoutMatcher = re.compile("Error: Timeout after ([0-9]+)")
 completedMatcher = re.compile("Elapsed time ([0-9]+\.[0-9]+).  Conjunction result ([0-9]+)")
 rootMatcher = re.compile("-(m[0-9]+\+[0-9]+)-")
 
+def resolve(x):
+    if not uniformTime:
+        return x
+    return resolution * int(x/resolution)
+
 def getInitial(line):
     m = initialMatcher.match(line)
     if m:
         conjunctCount = totalConjuncts
-        elapsed = float(m.group(1))
+        elapsed = resolve(float(m.group(1)))
         combinedSize = int(m.group(2))
         maxSize = int(m.group(3))
         computedSize = maxSize
-        return {"type" : "partial", "elapsed" : elapsed, "conjuncts" : conjunctCount, "max" : maxSize, "combined" : combinedSize, "computed" : computedSize }
+        return {"type" : "initial", "elapsed" : elapsed, "conjuncts" : conjunctCount, "max" : maxSize, "combined" : combinedSize, "computed" : computedSize }
 
 def getPartial(line):
     m = partialMatcher.match(line)
     if m:
-        elapsed = float(m.group(1))
+        elapsed = resolve(float(m.group(1)))
         conjunctCount = int(m.group(2))
         maxSize = int(m.group(3))
         combinedSize = int(m.group(4))
@@ -52,7 +77,7 @@ def getPartial(line):
 def getZero(line):
     m = zeroMatcher.match(line)
     if m:
-        elapsed = float(m.group(1))
+        elapsed = resolve(float(m.group(1)))
         conjunctCount = int(m.group(2))
         combinedSize = 1
         computedSize = 1
@@ -63,14 +88,14 @@ def getZero(line):
 def getTimeout(line):
     m = timeoutMatcher.match(line)
     if m:
-        time = int(m.group(1))
+        time = resolve(int(m.group(1)))
         return {"type" : "timeout", "elapsed" : time }
     return None
         
 def getCompleted(line):
     m = completedMatcher.match(line)
     if m:
-        elapsed = float(m.group(1))
+        elapsed = resolve(float(m.group(1)))
         resultSize = int(m.group(2))
         maxSize = resultSize
         combinedSize = resultSize
@@ -78,21 +103,23 @@ def getCompleted(line):
     return None
 
 def processLine(line):
+    global timePoints, maxTime
     d = getInitial(line)
+    if d is None:
+        d = getPartial(line)
+    if d is None:
+        d = getZero(line)
+    if d is None:
+        d = getTimeout(line)
+    if d is None:
+        d = getCompleted(line)
     if d is not None:
-        return d
-    d = getPartial(line)
-    if d is not None:
-        return d
-    d = getZero(line)
-    if d is not None:
-        return d
-    d = getTimeout(line)
-    if d is not None:
-        return d
-    d = getCompleted(line)
-    if d is not None:
-        return d
+        if 'elapsed' in d:
+            elapsed = d['elapsed']
+            if recordTime and elapsed not in timePoints:
+                timePoints.append(elapsed)
+                maxTime = max(maxTime, elapsed)
+    return d
 
 def getRoot(fname):
     m = rootMatcher.search(fname)
@@ -123,51 +150,117 @@ def processFile(fname):
         t = d["type"]
         if t in ["initial", "partial"]:
             # Create event
-            e = { k : d[k] for k in ['elapsed', 'max', 'combined', 'computed'] }
-            conjunctCount = d['conjuncts']
-            resultDict['partials'][conjunctCount] = e
+            e = { k : d[k] for k in ['elapsed', 'max', 'combined', 'computed', 'conjuncts'] }
+            elapsed = d['elapsed']
+            resultDict['partials'][elapsed] = e
             lastE = e
         elif t in [ 'timeout' ]:
             resultDict['type'] = t
             # Create terminal event
+            elapsed = d['elapsed']
             conjunctCount -= 1
-            e = { k : d[k] for k in ['elapsed'] }
+            e = { 'elapsed' : elapsed, 'conjunct' : conjunctCount }
             if lastE is not None:
                 for k in ['max', 'combined', 'computed']:
                     e[k] = lastE[k]
-            resultDict['partials'][conjunctCount] = e
+            resultDict['partials'][elapsed] = e
             break
         elif t in [ 'zero', 'completed' ]:
             resultDict['type'] = t
             # Create terminal event
             e = { k : d[k] for k in ['elapsed', 'max', 'combined', 'computed'] }
+            elapsed = d['elapsed']
             conjunctCount -= 1
-            resultDict['partials'][conjunctCount] = e
+            e['conjuncts'] = conjunctCount
+            resultDict['partials'][elapsed] = e
             break
         else:
             print("Unknown line type '%s'" % t)
     f.close()
     return resultDict
 
-def generateOutput(fileDict):
+def generateUniformTimes():
+    global timePoints
+    # Create new set of time points
+    ntimes = int(maxTime/resolution) + 1
+    timePoints = [resolution * i for i in range(ntimes)]
+    ifields = [""]
+    ifields += [floatFormat % t for t in timePoints]
+    print("\t".join(ifields))    
+
+def generateUniformOutput(fileDict):
     partials = fileDict['partials']
-    keys = list(partials.keys())
-    keys.sort()
-    keys.reverse()
-    ifields = ["", ""]
-    ifields += ["%.1f" % partials[k]['elapsed'] for k in keys]
-    print("\t".join(ifields))
-    vfields = [fileDict['type'], fileDict['root']]
-    vfields += [str(partials[k][reportField]) for k in keys]
+    tfields = ["" for t in timePoints]
+    for i in range(len(timePoints)):
+        t = timePoints[i]
+        if t in partials:
+            e = partials[t]
+            tfields[i] = str(e[reportField])
+        elif i > 0 and tfields[i-1] != "1":
+            tfields[i] = tfields[i-1]
+    label = fileDict['root'] + "-" + fileDict['type'][0]
+    vfields = [label] + tfields
     print("\t".join(vfields))
 
-def run(flist):
+def generateNonuniformOutput(fileDict):
+    partials = fileDict['partials']
+    timePoints = sorted(list(partials.keys()))
+    tfields = [""] + ["%.1f" % t for t in timePoints]
+    print("\t".join(tfields))
+    label = fileDict['root'] + "-" + fileDict['type'][0]
+    vfields = [label] + [str(partials[t][reportField]) for t in timePoints]
+    print("\t".join(vfields))
+
+def uniformRun(flist):
+    global recordTime
+    recordTime = True
+    # Pass one:  Gather all time points
     for fname in flist:
         dict = processFile(fname)
-        if dict is not None:
-            generateOutput(dict)
+    generateUniformTimes()
+    recordTime = False
+    for fname in flist:
+        dict = processFile(fname)
+        if dict is not None and dict['type'] != 'unknown':
+            generateUniformOutput(dict)
+
+def nonuniformRun(flist):
+    global recordTime
+    recordTime = False
+    for fname in flist:
+        dict = processFile(fname)
+        if dict is not None and dict['type'] != 'unknown':
+            generateNonuniformOutput(dict)
+
+
+
+def run(name, args):
+    global reportField, uniformTime
+    optlist, nargs = getopt.getopt(args, 'hur:')
+    for (opt, val) in optlist:
+        if opt == '-h':
+            usage(name)
+            return
+        elif opt == '-u':
+            uniformTime = True
+        elif opt == '-r':
+            if val == 'a':
+                reportField = 'combined'
+            elif val == 'm':
+                reportField = 'max'
+            elif val == 'c':
+                reportField = 'computed'
+            else:
+                print("Reporting field must be a, m, or c")
+                return
+        else:
+            print("Unknown option '%s'" % opt)
+            return
+    if uniformTime:
+        uniformRun(nargs)
+    else:
+        nonuniformRun(nargs)
 
 if __name__ == "__main__":
-    run(sys.argv[1:])
-
+    run(sys.argv[0], sys.argv[1:])
     
