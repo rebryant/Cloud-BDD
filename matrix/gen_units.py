9eeb77aba1dcc54d8d943629d194223031f443e6
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2020-02-03 14:54:47 -0500

    Enabled processing of multiple files

--- matrix/gen_units.py
+++ matrix/gen_units.py
@@ -11,18 +11,25 @@ dim = (3,3,3)
 auxCount = 23
 
 def usage(name):
-    print("%s: [-h] [-n (N|N1:N2:N3)] [-p AUX] (-L LFILE|-P PFILE) [-o UFILE")
+    print("%s: [-h] [-n (N|N1:N2:N3)] [-p AUX] FILE.{lit,exp} ...")
     print(" -h               Print this message")
     print " -n N or N1:N2:N3 Matrix dimension(s)"
     print(" -p AUX           Number of auxiliary variables")
-    print(" -L LFILE         Input literal file")
-    print(" -P PFILE         Input polynomial file")
-    print(" -o UFILE         Output unit file")
+    print("Files with suffix '.lit' consist of literals")
+    print("Files with suffix '.exp' consist of polynomial representations")
+    print("The results will be in files of the form FILE.units")
 
-def fromAssignment(asst, outfile):
+def generateFromAssignment(asst, outname):
+    try:
+        outfile = open(outname, 'w')
+    except Exception as ex:
+        print("Couldn't open file '%s' (%s)" % (outname, str(ex)))
+        return
     units = asst.generateUnitClauses(dim)
     for u in units:
         outfile.write("%d 0\n" % u)
+    outfile.close()
+    print("Generated file %s" % outname)
 
 def getFromLiterals(fname):
     try:
@@ -43,25 +50,11 @@ def getFromPolynomial(fname):
 def run(name, args):
     global dim, auxCount
     n1, n2, n3 = 3, 3, 3
-    fname = None
-    isPolynomial = False
-    outfile = sys.stdout
-    optlist, args = getopt.getopt(args, 'hn:p:L:P:o:')
+    optlist, args = getopt.getopt(args, 'hn:p:')
     for (opt, val) in optlist:
         if opt == '-h':
             usage(name)
             return
-        elif opt == '-L':
-            fname = val
-        elif opt == '-P':
-            isPolynomial = True
-            fname = val
-        elif opt == '-o':
-            try:
-                outfile = open(val, 'w')
-            except Exception as ex:
-                print("Couldn't open output file '%s' (%s)" % (val, str(ex)))
-                outfile = None
         elif opt == '-p':
             auxCount = int(val)
         elif opt == '-n':
@@ -74,18 +67,21 @@ def run(name, args):
                 print "Invalid matrix dimension '%s'" % val
                 usage(name)
                 return
-    if fname is None:
-        print("Must have input literal or polynomial file")
-        usage(name)
-        return
-    if outfile is None:
-        return
+
     dim = (n1, n2, n3)
-    asst = getFromPolynomial(fname) if isPolynomial else getFromLiterals(fname)
-    if asst is not None:
-        fromAssignment(asst, outfile)
-    if outfile != sys.stdout:
-        outfile.close()
+
+    for fname in args:
+        fields = fname.split(".")
+        extension = fields[-1]
+        root = ".".join(fields[:-1])
+        outname = root + ".units"
+        asst = None
+        if extension == 'lit':
+            asst = getFromLiterals(fname)
+        elif extension == '.exp':
+            asst = getFromPolynomial(fname)
+        if asst is not None:
+            generateFromAssignment(asst, outname)
     
 if __name__ == "__main__":
     run(sys.argv[0], sys.argv[1:])
