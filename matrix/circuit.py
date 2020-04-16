6b8fb1f5194d344d64bfe82c7e242fd1d1669049
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2020-04-16 18:09:40 -0400

    Enable randomizing ordering of conjuncts

--- matrix/circuit.py
+++ matrix/circuit.py
@@ -2,6 +2,7 @@
 # Assume constants one & zero are named "one" and "zero"
 
 import sys
+import random
 
 
 # Class to define use of ZDDs
@@ -262,9 +263,14 @@ class Circuit:
         ls.extend(argList)
         self.cmdLine("and", ls)
 
-    def conjunctN(self, dest, argList):
+    def conjunctN(self, dest, argList, randomize = False):
         ls = [dest]
-        ls.extend(argList)
+        if randomize:
+            args = list(argList)
+            random.shuffle(args)
+        else:
+            args = argList
+        ls.extend(args)
         self.cmdLine("conjunct", ls)
 
     def orN(self, dest, argList):
